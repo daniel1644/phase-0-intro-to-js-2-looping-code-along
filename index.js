6bfd7e56aef90b482599b6c898237b71f6340080
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-01 14:27:03 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let thankYouMessages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      thankYouMessages.push(message);
+      debugger;
+    }
+  
+    return thankYouMessages;
+  }
+  
+  // Example usage:
+  const names = ["Charlie", "Samip", "Ali"];
+  const eventName = "birthday";
+  const messages = writeCards(names, eventName);
+  console.log(messages);
+
+
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  // Example usage:
+countDown(10);
